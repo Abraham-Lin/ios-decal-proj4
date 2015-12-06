3a103bc2cc0e936afdac6ba4c5ea3ffe32139a79
Cindy Tung <cindytung@berkeley.edu>
2015-12-05 17:26:39 -0800

    gamefeed view

--- CalSportsNetwork/CalSportsNetwork/GamesFeedViewController.swift
+++ CalSportsNetwork/CalSportsNetwork/GamesFeedViewController.swift
@@ -8,27 +8,31 @@
 
 import UIKit
 
-class GamesFeedViewController: UIViewController {
+class GamesFeedViewController: UIViewController, UITableViewDelegate {
 
+    var cellContent = ["Mens Basketball", "Womens Basketball", "Mens Football", "Womens Volleyball"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let margins = view.layoutMarginsGuide
-        gameView = UIView(frame: UIScreen.mainScreen().bounds)
-        view.backgroundColor = UIColor.blueColor()
         
-        loadVisualElements()
-    }
-    
-    func loadVisualElements() {
-        let width = UIScreen.mainScreen().bounds.size.width
-        let height = UIScreen.mainScreen().bounds.size.height
-        let offset = height - width
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @available(iOS 2.0, *)
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+    
+    @available(iOS 2.0, *)
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Sports-Cell")
+        cell.textLabel?.text = cellContent[indexPath.row]
+        return cell
     }
     
     
