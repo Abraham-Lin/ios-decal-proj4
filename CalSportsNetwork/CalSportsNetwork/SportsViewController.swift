a53aafff97514e7c842a0b177b342662e460ad54
Abraham Lin <abraham_lin1996@berkeley.edu>
2015-12-05 16:51:23 -0800

    SportsViewController

--- CalSportsNetwork/CalSportsNetwork/SportsViewController.swift
+++ CalSportsNetwork/CalSportsNetwork/SportsViewController.swift
@@ -8,20 +8,31 @@
 
 import UIKit
 
-class SportsViewController: UIViewController {
-
+class SportsViewController: UIViewController, UITableViewDelegate {
+    
+    var cellContent = ["Mens Basketball", "Womens Basketball", "Mens Football", "Womens Volleyball"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let margins = view.layoutMarginsGuide
-        gameView = UIView(frame: UIScreen.mainScreen().bounds)
-        view.backgroundColor = UIColor.blueColor()
         
-        loadVisualElements()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @available(iOS 2.0, *)
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+    
+    @available(iOS 2.0, *)
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Sports-Cell")
+        cell.textLabel?.text = cellContent[indexPath.row]
+        return cell
+    }
+    
 }
